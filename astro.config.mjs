73b011f995514425b6ee4c35aad854d8027c7032
Ajay Jadhav <jadhavajay@gmail.com>
2023-06-03 01:47:35 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,23 @@ import deno from "@astrojs/deno";
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-const DEV_PORT = 8080;
+const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.CI
-		? 'https://themesberg.github.io'
-		: `http://localhost:${DEV_PORT}`,
-	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+	site: 'useful-rabbit-56.deno.dev',
+	//base: process.env.CI ? '/' : undefined,
 
 	output: 'server',
 	adapter: deno(),
 
 	/* Like Vercel, Netlify,… Mimicking for dev. server */
-	// trailingSlash: 'always',
+	//trailingSlash: 'always',
 
-	// server: {
+	server: {
 	// 	/* Dev. server only */
-	// 	port: DEV_PORT,
-	// },
+		port: DEV_PORT,
+	},
 
 	integrations: [
 		//
